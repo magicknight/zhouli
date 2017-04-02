d48eaf71547c292e0268bc7921dcd5284f711e2f
magicknight <liangzhihua@gmail.com>
2017-04-02 02:11:55 -0500

    coding

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
 # python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
 
-pip3 install src/requirements.txt
+pip3 install -r src/requirements.txt
 python3 ./src/process_log.py
